bbe7d8799ced2594d7ddee5ab7f0a28333a943ed
Aaron Saunders <aaron.marc.saunders@gmail.com>
2016-09-16 16:59:31 +0200

    calculations init

---
+++ R/calculations.R
@@ -0,0 +1,23 @@
+library(dplyr)
+library(stringr)
+
+calc_concentration <- function(qpcrdata) {
+  #' Parse the text output from a Statagene qPCR machine.
+  #'
+  #' @param qpcrdata dataframe
+  #' @return mutated dataframe
+  #' @examples
+  #' \dontrun{
+  #' calc_concentration(df)
+  #' }
+}
+calc_LOD <- function(qpcrdata) {
+  #' Parse the text output from a Statagene qPCR machine.
+  #'
+  #' @param qpcrdata dataframe
+  #' @return mutated dataframe
+  #' @examples
+  #' \dontrun{
+  #' calc_concentration(df)
+  #' }
+}
